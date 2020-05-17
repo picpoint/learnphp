962606decded019ac4ea2292f86bb3b1f1b4551b
picpoint <picpoint@yandex.ru>
2020-05-17 19:29:33 +0300

    work with 2 task

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php 
+
+
+function summNumb($x, $y) {
+  return $x + $y;
+}
+
+
+assert(1 === summNumb(0, 1));
+assert(1 == summNumb(1, 0));
+
+
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Home page</title>
+</head>
+<body>
+  
+  <h3>Hello world</h3>
+    
+    <?php
+      
+      include __DIR__ . '/functions.php';
+
+      echo(summNumb(1, 1));
+
+
+    ?>  
+
+</body>
+</html>

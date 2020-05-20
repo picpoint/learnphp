0dc16e9c001a4734175efd0500c137f37c75d025
picpoint <picpoint@yandex.ru>
2020-05-20 22:58:17 +0300

    work with 2 task

--- dist/index.php
+++ dist/index.php
@@ -159,46 +159,53 @@
 
     <div class="wrp__task2">
       <h3>Task 2</h3>
-      <div class="wrp__f1">
-        <span><input type="text" name="a" placeholder="a">x<sup>2</sup> + <input type="text" name="b" placeholder="b">x + <input type="text" name="c" placeholder="c"> = 0</span>
-      </div>
-      <div class="wrp__f2">
-        <span>D = b<sup>2</sup> - 4ac =</span> <span class="outdisc">
-          <?php
-            echo(discreminant(2, 5, -7));            
-          ?>
-        </span>
-      </div>
-      
-      <div class="wrp__f3">
-        <div class="wrp__f3x1">
-          <span>X1 = </span>
+      <form action="index.php" method="POST" class="wrp__formsend">
+        <div class="wrp__f1">        
+          <span>
+            <input type="text" name="a" placeholder="a">x<sup>2</sup> + 
+            <input type="text" name="b" placeholder="b">x + 
+            <input type="text" name="c" placeholder="c"> = 0
+          </span>            
         </div>
-        <div class="wrp__fraction">
-          <span> -b -&#8730D</span>
-          <span>2a</span>
+        <div class="wrp__f2">
+          <span>D = b<sup>2</sup> - 4ac =</span> <span class="outdisc">
+            <?php                            
+              echo(discreminant());
+            ?>
+          </span>
         </div>
-        <div class="wrp__f3x1out">
-          <span>=</span>
-          <span>?</span>
+        
+        <div class="wrp__f3">
+          <div class="wrp__f3x1">
+            <span>X1 = </span>
+          </div>
+          <div class="wrp__fraction">
+            <span> -b -&#8730D</span>
+            <span>2a</span>
+          </div>
+          <div class="wrp__f3x1out">
+            <span>=</span>
+            <span>?</span>
+          </div>
         </div>
-      </div>
 
-      <div class="wrp__f4">
-        <div class="wrp__f3x2">
-          <span>X2 = </span>
+        <div class="wrp__f4">
+          <div class="wrp__f3x2">
+            <span>X2 = </span>
+          </div>
+          <div class="wrp__fraction">
+            <span> -b +&#8730D</span>
+            <span>2a</span>
+          </div>
+          <div class="wrp__f3x2out">
+            <span>=</span>
+            <span>?</span>
+          </div>
         </div>
-        <div class="wrp__fraction">
-          <span> -b +&#8730D</span>
-          <span>2a</span>
-        </div>
-        <div class="wrp__f3x2out">
-          <span>=</span>
-          <span>?</span>
-        </div>
-      </div>
 
-      <button>Вычислить</button>
+        <button type="submit">Вычислить</button>
+        
+      </form>
 
     </div>
 
--- src/functions.php
+++ src/functions.php
@@ -31,12 +31,27 @@ function exor($x, $y) {
 
 
 
-function discreminant($a, $b, $c) {
-  $discrim = $b*$b - 4*$a*$c;
-  $x1 = (-$b - sqrt($discrim)) / 2*$a;
-  $x2 = (-$b - sqrt($discrim)) / 2*$a;
+function discreminant() {
+  
+  if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $a = $_POST['a'];
+    $b = $_POST['b'];
+    $c = $_POST['c']; 
+  
+    if(empty($a) || empty($b) || empty($c)) {
+      $a = $b = $c = 0; 
+      $discrim = $b*$b - 4*$a*$c;
+      $x1 = (-$b - sqrt($discrim)) / 2*$a;
+      $x2 = (-$b - sqrt($discrim)) / 2*$a;
+    } else {
+      $discrim = $b*$b - 4*$a*$c;
+      $x1 = (-$b - sqrt($discrim)) / 2*$a;
+      $x2 = (-$b - sqrt($discrim)) / 2*$a;
+    }    
 
-  return $discrim;
+    return $discrim;
+  }  
+
 }
 
 

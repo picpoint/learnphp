60284cf1e3ea21130a13fa2827cd57df165ac1cb
picpoint <picpoint@yandex.ru>
2020-05-29 23:40:20 +0300

    work with 2 task

--- dist/loadFiles.php
+++ dist/loadFiles.php
@@ -1,16 +1,16 @@
 <?php
 
 function loadFile() {
+  if(isset($_POST['actionbtn'])) {    
+    var_dump($_FILES["loadfield"]);        
+    var_dump($_FILES["loadfield"]["name"]);        
 
-  if(isset($_POST['actionbtn'])) {
-    $path = __DIR__."/pict";
-    var_dump($_FILES['loadfield']);
-    
-    if(0 == $_FILES['loadfield']['error']) {
-      $fl = $_FILES['loadfield']['tmp_name'];
+    if(0 == $_FILES["loadfield"]["error"]) {
+      $fl = $_FILES["loadfield"]["tmp_name"];
+      $path = __DIR__."pict";
       move_uploaded_file($fl, $path);
-    } else {
-      echo("some error");
     }
+
+
   }
 }

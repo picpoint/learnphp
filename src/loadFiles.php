5c1ed99d2fe3492097dead6ada0d4e65f4dca8fc
picpoint <picpoint@yandex.ru>
2020-05-30 00:26:40 +0300

    work with task 2

--- src/loadFiles.php
+++ src/loadFiles.php
@@ -2,12 +2,11 @@
 
 function loadFile() {
   if(isset($_POST['actionbtn'])) {    
-    var_dump($_FILES["loadfield"]);        
-    var_dump($_FILES["loadfield"]["name"]);        
+    var_dump($_FILES["loadfield"]);            
 
     if(0 == $_FILES["loadfield"]["error"]) {
       $fl = $_FILES["loadfield"]["tmp_name"];
-      $path = __DIR__."/pict".$_FILES['loadfield']['name'];
+      $path = __DIR__.'/pict/pict'.$_FILES['loadfield']['name'];
       move_uploaded_file($fl, $path);
     }
 

2ec3b83ecfa77240b823103fc5ce5636fcb06c11
picpoint <picpoint@yandex.ru>
2020-05-29 08:53:09 +0300

    work with first task

--- dist/functions.php
+++ dist/functions.php
@@ -2,46 +2,41 @@
 
 
 function readGuestBook() {
+  $file = __DIR__."/../guestbook.txt";
   
   if(isset($_POST['send'])) {
     if(!empty($_POST['nameuser']) && !empty($_POST['messageuser'])) {
       $nameuser = $_POST['nameuser'];
       $messageuser = $_POST['messageuser'];
-
-      echo($nameuser);
-      echo("<br>");
-      echo($messageuser);
+      $writeData = $nameuser.': '.$messageuser;
+      echo($writeData);
+      $writeFile = fopen($file, 'a');
+      fwrite($writeFile, $writeData);
+      fclose($writeFile);
     } else {
       echo('Fields is empty');
-    }
-    
-
-    
+    }    
   }
   
 
-  
-  
-  // $file = __DIR__."/../guestbook.txt";
+  if(file_exists($file)) {
+    $arr = file($file, FILE_IGNORE_NEW_LINES);    
 
-  // if(file_exists($file)) {
-  //   $arr = file($file, FILE_IGNORE_NEW_LINES);    
-
-  //   foreach($arr as $key => $value) {      
-  //     $res = explode(":", $value);      
-  //     $name = $res[0];
-  //     $message = $res[1];
-  //     $mass[$name] = $message;
+    foreach($arr as $key => $value) {      
+      $res = explode(":", $value);      
+      $name = $res[0];
+      $message = $res[1];
+      $mass[$name] = $message;
       
-  //     echo("<div class='wrp__block'>");      
-  //     echo('<span class="wrp__usr"><img src="pict/noname.jpg">'.$name.'</span>');
-  //     echo('<span class="wrp__msg">'.$message.'</span>');                        
-  //     echo("</div>");
-  //   }    
+      echo("<div class='wrp__block'>");      
+      echo('<span class="wrp__usr"><img src="pict/noname.jpg">'.$name.'</span>');
+      echo('<span class="wrp__msg">'.$message.'</span>');                        
+      echo("</div>");
+    }    
 
     
-  // } else {
-  //   echo("file not exists ...");
-  // }  
+  } else {
+    echo("file not exists ...");
+  }  
   
 }

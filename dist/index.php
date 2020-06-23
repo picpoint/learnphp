a0db5d4559c6e4a486bf6b7b1f5d165ba8338ddc
picpoint <picpoint@yandex.ru>
2020-06-23 16:15:15 +0300

    work with mvc project

---
+++ dist/index.php
@@ -0,0 +1,19 @@
+<?php
+  session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Tasks  on php</title>
+  <link rel="stylesheet" href="css/style.css">	  
+</head>
+<body>
+
+  <section class="wrp">        
+    <span>Hello</span>
+  </section>  
+
+</body> 
+</html>

d585e2d0f03d885e4ae8b7841e3517add0528e76
picpoint <picpoint@yandex.ru>
2020-06-29 16:48:05 +0300

    work with first task

--- models/readDBClass.php
+++ models/readDBClass.php
@@ -8,27 +8,34 @@ class ReadDB {
   protected $passdb;
   protected $db;
   protected $table; 
-  
+  public $dbh;
+    
   
   public function __construct() {
     $path = __DIR__.'/../config.txt';            
     $conf = file($path);
-    $host = $conf[0];
-    $usernamedb = $conf[1];
-    $passdb = $conf[2];
-    $db = $conf[3];
-    $table = $conf[4];
-
-    $dbh = new PDO('mysql:host=localhost;dbname=task8', 'rmtar', '2203');
-    var_dump($dbh);
+    $host = trim($conf[0]);
+    $usernamedb = trim($conf[1]);
+    $passdb = trim($conf[2]);
+    $db = trim($conf[3]);
+    $table = trim($conf[4]);
+    
+    $dbh = new PDO('mysql:host='.$host.';dbname='.$db.'', $usernamedb, $passdb);
+    var_dump($dbh);    
+        
     
   }
 
 
-  public function executeMet(string $sql) {
-    $sth = $dbh->prepare('SELECT * FROM template');
-    return $sth->execute();
+  public function executeMeth(string $sql) {
+    $sth = $dbh -> prepare($sql);
+    $sth -> execute();
+    $data = $sth -> fetchAll();
+    var_dump($data);
   }
+
+
+  
 
 
 

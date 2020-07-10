25035f1c523dc2466a24ac9db13aa56914983ad4
picpoint <picpoint@yandex.ru>
2020-07-10 16:42:29 +0300

    make adaptive burger

--- dist/js/burger.js
+++ dist/js/burger.js
@@ -2,16 +2,22 @@ let burger = document.querySelector('.wrp__burger');
 let list = document.querySelector('.wrp__list');
 let link = document.querySelector('.wrp__link');
 let enterblock = document.querySelector('.wrp__enter');
+let lineone = document.querySelector('.wrp__lineone');
+let linetwo = document.querySelector('.wrp__linetwo');
+let linethree = document.querySelector('.wrp__linethree');
 
 
 
 
 class Burger {
-	constructor(burger, list, link, enterblock) {
+	constructor(burger, list, link, enterblock, lineone, linetwo, linethree) {
 		this.burger = burger;
 		this.list = list;
 		this.link = link;
 		this.enterblock = enterblock;
+		this.lineone = lineone;
+		this.linetwo = linetwo;
+		this.linethree = linethree;
 	}
 
 
@@ -19,6 +25,10 @@ class Burger {
 		this.burger.addEventListener('click', () => {									
 			this.list.classList.toggle('listhght');
 			this.enterblock.classList.toggle('menutgl');
+			this.lineone.classList.toggle('linerotate');
+			this.linetwo.classList.toggle('hideelem');
+			this.linethree.classList.toggle('linerotate2');
+			
 
 			for(let i = 0; i < this.list.children.length; i++) {
 				console.log(this.list.children[i]);
@@ -28,8 +38,11 @@ class Burger {
 		});
 	}
 
+
+
+
 }
 
 
-let act = new Burger(burger, list, link, enterblock);
+let act = new Burger(burger, list, link, enterblock, lineone, linetwo, linethree);
 act.showHide();

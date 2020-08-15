01d249148bd54ba3d0aec06df763c94f955871ab
picpoint <picpoint@yandex.ru>
2020-08-15 16:06:11 +0300

    end work with galery and listen music

--- view/userPage.php
+++ view/userPage.php
@@ -2,10 +2,10 @@
   session_start();
   // setcookie($_SESSION['login'], $_SESSION['password'], time() + 86400);
 
-  error_reporting(0);
-  // ini_set('error_reporting', E_ALL);
-  // ini_set('display_errors', 1);
-  // ini_set('display_startup_errors', 1);
+  // error_reporting(0);
+  ini_set('error_reporting', E_ALL);
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
 
 ?>
 
